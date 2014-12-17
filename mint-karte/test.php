134982ca34f29fdb4704611e1d979fda7865a122
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2014-12-17 12:06:21 +0100

    aktueller Arbeitsstand

--- mint-karte/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$foaf = new EasyRdf_Graph("http://njh.me/foaf.rdf");
-$foaf->load();
-$me = $foaf->primaryTopic();
-echo "My name is: ".$me->get('foaf:name')."\n";
-?>
