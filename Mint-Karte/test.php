fe4da2aff4ee63ca71ca4026d6a0673961fdfc6a
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-01-11 22:45:14 +0100

    weiteres Beispiel Mint-Karte/test.php sowie Aufzeichnungen zu den Treffen ergänzt

--- Mint-Karte/test.php
+++ Mint-Karte/test.php
@@ -1,8 +1,20 @@
 <?php
 require 'vendor/autoload.php';
 
-$foaf = new EasyRdf_Graph("http://njh.me/foaf.rdf");
-$foaf->load();
+/* 11.01.2015, HGG: Gefixt */
+
+$graph = new EasyRdf_Graph("http://leipzig-data.de/Data/MINTBroschuere2012/");
+// Parameter ist der Name des RDF Graphen
+
+$graph->parseFile("data/MINTBroschuere2012.ttl");
+// Name der Datei, aus der dieser RDF-Graph geladen werden soll
+
+echo $graph->dump("html"); // erzeugt einen html view eines RDF Dumps
+
+/* 
+
 $me = $foaf->primaryTopic();
 echo "My name is: ".$me->get('foaf:name')."\n";
+
+*/
 ?>
