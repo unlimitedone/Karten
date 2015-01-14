2fc428e91f90f90571905b00f2d35ab4a449cc03
Hans-Gert Graebe <graebe@informatik.uni-leipzig.de>
2015-01-14 12:51:48 +0100

    Aufgeräumt und Aufzeichnungen ergänzt

--- Jugendstadtplan/Plath/public/test.php
+++ Jugendstadtplan/Plath/public/test.php
@@ -16,13 +16,13 @@ ini_set('default_charset', 'utf-8');
 
 Aufruf "php test.php >o.html"
 
-Linke vorher Karten/Jugendstadtplan/RDF-Data ans Verzeichnis Plath/public
+Emanuels json-Datei in Trutle verwandeln
 
 */
 
-$graph1 = new EasyRdf_Graph("http://leipzig-data.de/Data/JSP/TeilGraph/");
-$graph1->parseFile("RDFData/Orte.ttl");
-echo $graph1->dump("html");
+$graph = new EasyRdf_Graph("http://leipzig-data.de/Data/JSP-Alt/");
+$graph->parseFile("data/jugendstadtplan.json");
+echo $graph->serialise("turtle");
 
 ?>
 
